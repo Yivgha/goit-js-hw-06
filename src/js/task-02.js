77f86ad6d7d09b184370af29d7cb148aca8f8003
Yevheniia P <90108689+Yivgha@users.noreply.github.com>
2022-05-01 23:32:43 +0300

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsItems = ingredients.map(ingredient => {
+
+  const elName = document.createElement("li");
+  elName.textContent = ingredient;
+
+  elName.classList.add("item");
+  return elName;
+});
+
+const ingredientsList = document.querySelector("#ingredients");
+ingredientsList.append(...ingredientsItems);
+
+console.log(ingredientsList);
