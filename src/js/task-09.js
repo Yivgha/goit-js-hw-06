447e2b991a3ae9603155afd9036128e0252398c7
Yevheniia P <90108689+Yivgha@users.noreply.github.com>
2022-05-03 01:12:33 +0300

    task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
+const btnChangeColor = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
+function onChangeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+}
+
+btnChangeColor.addEventListener("click", onChangeColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
