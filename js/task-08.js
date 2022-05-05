0698d48aa61f661df7bb6a20a9fd68f637d04f6d
Yevheniia P <90108689+Yivgha@users.noreply.github.com>
2022-05-06 00:29:37 +0300

    task8 edited

--- js/task-08.js
+++ js/task-08.js
@@ -12,7 +12,7 @@ function handleSubmit(event) {
         return alert("Please fill in all the fields!");
     }
 
-    console.log(`Email: ${email.value}, Password: ${password.value}`);
-  event.currentTarget.reset();
+    console.log({ Email: email.value, Password: password.value });
+  form.reset();
 }
 
